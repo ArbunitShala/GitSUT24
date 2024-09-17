ab54e2c956f9e82f16912b5586cdd0dbc19382e3
ArbunitShala <arbunitshalaa@gmail.com>
2024-09-17 11:37:41 +0200

    Added username choice and greeting

--- GitSUT24/Program.cs
+++ GitSUT24/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to this amazing game!");
+            Console.WriteLine("Choose a nickname:");
+            string userNick = Console.ReadLine();
+            Console.WriteLine($"Greetings, {userNick}!");
+
+            Console.WriteLine("Ny text");
         }
     }
 }
